76aae7fa07ffccd86218efb0bdfe2c4ab3b86cfc
Sibidas <sibidas_sivadas@intuit.com>
2024-04-13 20:01:10 +0530

    add: historical data api

--- utils.go
+++ utils.go
@@ -30,6 +30,7 @@ const (
 	URILTP              string = "rest/secure/angelbroking/order/v1/getLtpData"
 	URIRMS              string = "rest/secure/angelbroking/user/v1/getRMS"
 	URIConvertPosition  string = "rest/secure/angelbroking/order/v1/convertPosition"
+	URIHistorical       string = "rest/secure/angelbroking/historical/v1/getCandleData"
 )
 
 func structToMap(obj interface{}, tagName string) map[string]interface{} {
